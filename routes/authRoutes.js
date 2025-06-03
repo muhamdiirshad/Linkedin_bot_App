b52b9714ea05a4151a2f0aed179f3366124915c7
Kavita wani <113329083+kavita346@users.noreply.github.com>
2025-06-03 13:00:00 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// 1. Auth Routes
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
-router.post("/signup",  authController.signup);
-router.post("/login",  authController.login);
+// 3. Password Reset Routes
+router.post("/request-reset-password", authController.requestResetPassword);
+router.get("/validate-reset-token",  authController.validateResetToken);
+router.post("/reset-password",  authController.resetPassword);
 
 module.exports = router;
