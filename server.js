df972703225062b75d1e7a39cf45395640f9d09c
Vikas Singh <44vikasrajput44@gmail.com>
2025-06-11 20:22:17 +0530

    add email system

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const connectDB = require('./config/db');
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json()); 
+app.use(bodyParser.json({ limit: '5mb' }));
 
 console.log("JWT_SECRET:", process.env.JWT_SECRET); // ✅ This will now log the correct value
 
@@ -14,6 +15,8 @@ console.log("JWT_SECRET:", process.env.JWT_SECRET); // ✅ This will now log the
 const postRoutes = require('./routes/postRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const scheduledRoutes = require('./routes/scheduledRoutes');
+const { sendBulkEmail } = require('./controllers/emailController');
+
 
 // ✅ Auth Route
 const authRoutes = require('./routes/authRoutes');
@@ -28,6 +31,7 @@ app.use('/api/post', postRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use('/api/scheduler', scheduledRoutes);
 app.use('/api/auth', authRoutes); // ✅ Auth routes added
+app.post('/api/send-email', sendBulkEmail);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
